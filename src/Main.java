f136c20ed6683949c50f66821b7396be6ce0bde9
andrew <kimyosu@protonmail.com>
2025-10-03 02:44:29 -0300

    Criando um SellerDao

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJdbc;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +14,8 @@ public class Main {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", localDate,
                 3000.0, obj);
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
