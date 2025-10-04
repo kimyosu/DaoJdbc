16fe987b033a340860265971c11ccfd252b0bd98
andrew <kimyosu@protonmail.com>
2025-10-03 23:59:21 -0300

    Adicionando novo teste(findAll)

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,12 @@ public class Main {
         System.out.println("\n=== TEST 2: seller findByDepartment  ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartmentId(department);
-
         list.forEach(System.out::println);
+
+        System.out.println("\n\n=== TEST 3: seller findAll  ===");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
+
 
         DB.closeConnection();
 
