cad46b156edfb4b6f4ea3d18fa928d761dce41e3
andrew <kimyosu@protonmail.com>
2025-10-04 03:02:21 -0300

    Adicionando outro construtor

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -11,6 +11,11 @@ public class Department implements Serializable {
     private String name;
 
     public Department() {}
+
+    public Department(String name) {
+        this.name = name;
+    }
+
     public Department(Integer id, String name) {
         this.id = id;
         this.name = name;
