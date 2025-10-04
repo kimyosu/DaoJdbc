2345559b4d31bbbf13db166417cece0f6812f2e4
andrew <kimyosu@protonmail.com>
2025-10-03 23:02:07 -0300

    Adicionando novo teste

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,10 +12,19 @@ public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
 
+        System.out.println("=== TEST 1: seller findById ===");
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment  ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartmentId(department);
+
+        list.forEach(System.out::println);
+
+        DB.closeConnection();
+
 
     }
 }
